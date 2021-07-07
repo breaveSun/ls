161b93d47fe3ddc3ac1c69e06eb5ac22ca4f98b5
meihui <2528280083@qq.com>
2021-07-08 00:08:13 +0800

    ccc

--- go.mod
+++ go.mod
@@ -11,13 +11,10 @@ require (
 	github.com/go-playground/universal-translator v0.17.0
 	github.com/go-playground/validator/v10 v10.6.1
 	github.com/go-redis/redis/v8 v8.11.0
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gomodule/redigo v1.8.5 // indirect
-	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/spf13/viper v1.8.1 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
 	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
@@ -27,5 +24,4 @@ require (
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- internal/app/plantform_tool/lib/log.go
+++ internal/app/plantform_tool/lib/log.go
@@ -11,8 +11,7 @@ import (
 
 //只能输出结构化日志，但是性能要高于 SugaredLogger
 var logger *zap.Logger
-//可以输出 结构化日志、非结构化日志。性能茶语 zap.Logger，具体可见上面的的单元测试
-var sugarLogger *zap.SugaredLogger
+
 func InitLog(){
 	config := zapcore.EncoderConfig{
 		MessageKey:   "msg",  //结构化（json）输出：msg的key
@@ -29,24 +28,31 @@ func InitLog(){
 		},//输出时间戳格式
 	}
 	//自定义日志级别：自定义Info级别
+	debugLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.DebugLevel
+	})
+
+	//自定义日志级别：自定义Info级别
 	infoLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl < zapcore.WarnLevel && lvl >= logLevel
+		return lvl >= zapcore.InfoLevel && lvl < zapcore.ErrorLevel
 	})
 
 	//自定义日志级别：自定义Warn级别
-	warnLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl >= zapcore.WarnLevel && lvl >= logLevel
+	errLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
 	})
 	infoWriter := getWriter("/info")
 	warnWriter := getWriter("/err")
 	// 实现多个输出
 	core := zapcore.NewTee(
-		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(infoWriter), infoLevel), //将info及以下写入logPath，NewConsoleEncoder 是非结构化输出
-		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(warnWriter), warnLevel),//warn及以上写入errPath
-		zapcore.NewCore(zapcore.NewJSONEncoder(config), zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout)), logLevel),//同时将日志输出到控制台，NewJSONEncoder 是结构化输出
+		//将info及以下写入logPath，NewConsoleEncoder 是非结构化输出
+		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(infoWriter), infoLevel),
+		//warn及以上写入errPath
+		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(warnWriter), errLevel),
+		//同时将日志输出到控制台，NewJSONEncoder 是结构化输出
+		zapcore.NewCore(zapcore.NewJSONEncoder(config), zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout)), debugLevel),
 	)
-	logger = zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.WarnLevel))
-	sugarLogger = logger.Sugar()
+	logger = zap.New(core, zap.AddCaller())
 }
 
 func getWriter(filename string) io.Writer {
